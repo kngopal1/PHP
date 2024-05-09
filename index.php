e5c80c9492ccfe973fd2de5f7b2dccb7956411ed
Nandagopal Kamaraj <nandgopal@rasheedfoundation.org>
2024-05-09 17:28:35 +0530

    PHP

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My First PHP Page</title>
+</head>
+<body>
+    <h1>Welcome to My First PHP Page</h1>
+    <p>Hello, World</p>
+    <p>Current Date and Time :
+    <?php
+    echo date("d-m-Y H:i:s");
+    ?>
+    </p>
+</body>
+</html>
